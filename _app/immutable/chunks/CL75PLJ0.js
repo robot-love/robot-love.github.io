9b424e9eab520ff5f9b4f20bf59dfa72296fbce1
Sepehr Ghavam <sepehr@robotlove.dev>
2025-08-01 21:49:45 -0700

    Updates

---
+++ _app/immutable/chunks/CL75PLJ0.js
@@ -0,0 +1,5 @@
+function f(i,u){const n={},a={},r={$$scope:1};let s=i.length;for(;s--;){const t=i[s],e=u[s];if(e){for(const o in t)o in e||(a[o]=1);for(const o in e)r[o]||(n[o]=e[o],r[o]=1);i[s]=e}else for(const o in t)r[o]=1}for(const t in a)t in n||(n[t]=void 0);return n}const b="https://robot-love.github.io",c="Sepehr",d=`${c}`,g="/robotlove.png",l=`I’m ${c}, a software engineer from Guelph, Ontario, currently based in Seattle, WA.
+
+Presently I work on core infra @ Meta. I did some graduate research in computer vision and
+image processing. I like Rust.
+`,p="robot-love/robot-love.github.io";export{g as a,l as b,f as c,p as g,d as n,b as w};
--- _app/immutable/entry/start.CMGXA1u8.js
+++
@@ -1,1 +0,0 @@
-import{l as o,b as r}from"../chunks/CZ9WQ9xH.js";export{o as load_css,r as start};
